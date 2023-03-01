86042b34892b944341ccfecbdbe972ef7de9635b
uchar-9 <2357996956@qq.com>
2023-03-01 10:57:39 +0800

    前端第一次上传

---
+++ src/views/common/home.vue
@@ -0,0 +1,146 @@
+<template>
+  <div class="mod-home">
+    <div class="home-cards">
+      <el-carousel :interval="4000" type="card" height="300px">
+        <el-carousel-item v-for="item in cardList" :key="item.id" align="center">
+          <h3><img :src="item.src" alt="" style="height:400px; width:700px"  /></h3>
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+    <div class="home-view">
+      <el-row>
+        <el-col :span="12" v-for="item in viewList" :key="item.id">
+          <el-card :body-style="{ padding: '0px', height: '650px' }">
+            <div class="home-title">
+              <h1>{{item.title}}</h1>
+            </div>
+            <img :src="item.src" class="image" style="height: 400px;width: 600px">
+            <div style="padding: 14px;">
+              <span class="text">{{item.text}}</span>
+              <div class="bottom clearfix">
+                <el-button type="primary" class="button" round>{{item.button}}</el-button>
+              </div>
+            </div>
+          </el-card>
+        </el-col>
+      </el-row>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        cardList: [
+          {id: 1, src: require('@/assets/img/card/card1.jpg')},
+          {id: 2, src: require('@/assets/img/card/card2.jpg')},
+          {id: 3, src: require('@/assets/img/card/card3.jpg')},
+          {id: 4, src: require('@/assets/img/card/card4.jpg')},
+          {id: 5, src: require('@/assets/img/card/card5.jpg')},
+          {id: 6, src: require('@/assets/img/card/card6.jpg')}
+        ],
+        viewList: [
+          { id: 1,
+            src: require('@/assets/img/view/registered.jpg'),
+            title: '爱口腔，爱健康',
+            text: '快速挂号，预约医生，马上就诊！',
+            button: '预约挂号'
+          },
+          { id: 2,
+            src: require('@/assets/img/view/department.jpg'),
+            title: '专业，让您的口腔爱上美',
+            text: '牙体牙髓科:治疗龋病、牙本质过敏症、慢性磨耗、楔状缺损、牙髓炎、根尖周炎等硬组织疾病;\n' +
+              '牙周病科:治疗牙龈、牙槽骨、牙周膜等支持组织疾病;\n' +
+              '口腔黏膜病科:治疗复发性口腔溃疡、扁平苔藓等口腔黏膜疾病;\n' +
+              '口腔颌面外科:拔除牙齿或治疗口腔囊肿、外伤、肿瘤等疾病的科室;\n' +
+              '口腔修复科针对牙体缺损或牙列缺失进行治疗的科室;\n' +
+              '口腔正畸科:治疗牙列不齐、前突等错(牙合)畸形的科室；\n' +
+              '14岁以下的患者儿童到牙病科就诊。',
+            button: '查看科室'
+          },
+          { id: 3,
+            src: require('@/assets/img/view/medical_record.jpg'),
+            title: '口腔医生是您口腔健康的好朋友',
+            text: '电子病历是新医疗体系的重要载体，信息学和医学科学相结合，是传统医学迈向现代医学的重要标志。',
+            button: '查看病历'
+          },
+          { id: 4,
+            src: require('@/assets/img/view/pharmaceuticals.jpg'),
+            title: '请带走您的健康口腔，留下我们共同的笑容',
+            text: '一尘不染，才能永保健康；一丝不敬，方能精品纷呈，产品质量好，全靠工作质量保。',
+            button: '查询药物'
+          },
+          { id: 5,
+            src: require('@/assets/img/view/bill.jpg'),
+            title: '自信的微笑，更美丽的你',
+            text: '我院基本医疗服务项目是根据 《广东省发展改革委关于督促落实收费目录清单制度的通知》 （粤发改价格函［2019］340号）要求，公示我院医疗服务项目收费价格，具体医疗服务项目收费清单详见附件。',
+            button: '费用结算'
+          },
+          { id: 6,
+            src: require('@/assets/img/view/personnel.jpg'),
+            title: '一个微笑，闪耀出珍珠般的光芒',
+            text: '人员信息安全保证',
+            button: '人员信息'
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style>
+.mod-home {
+  line-height: 1.5;
+}
+.home-title{
+  text-align: center;
+  font-size: 15px;
+  color: #247ba8;
+  font-family: 方正舒体;
+}
+.text{
+  display: inline-block;
+  height: 100px;
+}
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 14px;
+  opacity: 0.75;
+  line-height: 200px;
+  margin: 0;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n+1) {
+  background-color: #d3dce6;
+}
+.bottom {
+  margin-top: 10px;
+  line-height: 12px;
+}
+
+.button {
+  padding: 0;
+  float: right;
+}
+
+.image {
+  width: 100%;
+  display: block;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+
+.clearfix:after {
+  clear: both
+}
+</style>
+
---
+++ src/views/ghyy/yy.vue
@@ -0,0 +1,219 @@
+<template>
+  <div class="mod-user">
+    <el-form :inline="true" :model="dataForm" @keyup.enter.native="getDataList()">
+      <el-form-item>
+        <el-input v-model="dataForm.patientName" placeholder="患者名" clearable @change="getDataList"></el-input>
+      </el-form-item>
+      <el-select v-model="dataForm.status" placeholder="是否过期">
+        <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value" @change="getDataList">
+        </el-option>
+      </el-select>
+      <el-form-item>
+        <el-date-picker
+          @change="dateSelect"
+          v-model="dataForm.selectDate"
+          type="datetimerange"
+          value-format="yyyy-MM-dd"
+          range-separator="至"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"/>
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="getDataList()">查询</el-button>
+        <el-button type="danger" @click="deleteHandle()" :disabled="dataListSelections.length <= 0">批量删除</el-button>
+      </el-form-item>
+    </el-form>
+    <el-table
+      :data="dataList"
+      border
+      v-loading="dataListLoading"
+      @selection-change="selectionChangeHandle"
+      style="width: 100%;">
+      <el-table-column type="selection" header-align="center" align="center" width="50"></el-table-column>
+      <el-table-column prop="ghId" header-align="center" align="center" width="80" label="预约ID"/>
+      <el-table-column prop="patientEntity.patientName" header-align="center" align="center" label="患者名"/>
+      <el-table-column prop="patientEntity.patientName" header-align="center" align="center" label="预约科室"/>
+      <el-table-column prop="patientEntity.patientName" header-align="center" align="center" label="预约医生"/>
+      <el-table-column prop="patientEntity.sex" header-align="center" align="center" width="50" label="性别">
+        <template slot-scope="scope">
+          <span style="margin-right: 10px;">{{ scope.row.patientEntity.sex == '0' ? '女' : '男' }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column prop="patientEntity.age" header-align="center" align="center" label="年龄"/>
+      <el-table-column prop="patientEntity.mobile" header-align="center" align="center" label="手机号"/>
+      <el-table-column prop="ghTime" header-align="center" align="center" width="180" label="预约时间"/>
+      <el-table-column prop="status" header-align="center" align="center" label="状态">
+        <template slot-scope="scope">
+          <el-tag v-if="scope.row.status === 0" size="small" type="danger">已就诊</el-tag>
+          <el-tag v-if="scope.row.status === 2" size="small" type="info">已过期</el-tag>
+          <el-tag v-if="scope.row.status === 1" size="small">有效</el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column fixed="right" header-align="center" align="center" width="150" label="操作">
+        <template slot-scope="scope">
+          <el-button v-if="scope.row.status !== 0 && scope.row.status !== 2" type="text" size="small" @click="updateHandle(scope.row.ghId)">已就诊</el-button>
+          <el-button type="text" size="small" @click="deleteHandle(scope.row.ghId)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-pagination
+      @size-change="sizeChangeHandle"
+      @current-change="currentChangeHandle"
+      :current-page="pageIndex"
+      :page-sizes="[10, 20, 50, 100]"
+      :page-size="pageSize"
+      :total="totalPage"
+      layout="total, sizes, prev, pager, next, jumper">
+    </el-pagination>
+    <!-- 弹窗, 新增 / 修改 -->
+    <add-or-update v-if="addOrUpdateVisible" ref="addOrUpdate" @refreshDataList="getDataList"></add-or-update>
+  </div>
+</template>
+
+<script>
+import AddOrUpdate from './gh-temp'
+export default {
+  data () {
+    return {
+      dataForm: {
+        patientName: '',
+        selectDate: [],
+        starDate: '',
+        endDate: '',
+        status: ''
+      },
+      options: [{value: '1', label: '有效'}, {value: '0', label: '已就诊'}, {value: '2', label: '已过期'}, {value: '', label: '全部状态'}],
+      dataList: [],
+      pageIndex: 1,
+      pageSize: 10,
+      totalPage: 0,
+      dataListLoading: false,
+      dataListSelections: [],
+      addOrUpdateVisible: false
+    }
+  },
+  components: {
+    AddOrUpdate
+  },
+  created () {
+    this.getDataList()
+  },
+  methods: {
+    // 获取数据列表
+    getDataList () {
+      this.dataListLoading = true
+      this.$http({
+        url: this.$http.adornUrl('/sys/gh/list'),
+        method: 'get',
+        params: this.$http.adornParams({
+          'page': this.pageIndex,
+          'limit': this.pageSize,
+          'patientName': this.dataForm.patientName,
+          'starDate': this.dataForm.starDate,
+          'endDate': this.dataForm.endDate,
+          'status': this.dataForm.status
+        })
+      }).then(({data}) => {
+        if (data && data.code === 0) {
+          this.dataList = data.list.records
+          this.totalPage = data.list.pages
+        } else {
+          this.dataList = []
+          this.totalPage = 0
+        }
+        this.dataListLoading = false
+      })
+    },
+    dateSelect () {
+      if (this.dataForm.selectDate === null) {
+        this.dataForm.starDate = ''
+        this.dataForm.endDate = ''
+      } else {
+        this.dataForm.starDate = this.dataForm.selectDate[0]
+        this.dataForm.endDate = this.dataForm.selectDate[1]
+      }
+    },
+    // 每页数
+    sizeChangeHandle (val) {
+      this.pageSize = val
+      this.pageIndex = 1
+      this.getDataList()
+    },
+    // 当前页
+    currentChangeHandle (val) {
+      this.pageIndex = val
+      this.getDataList()
+    },
+    // 多选
+    selectionChangeHandle (val) {
+      this.dataListSelections = val
+    },
+    // 新增
+    addOrUpdateHandle (patientId, patientName) {
+      this.addOrUpdateVisible = true
+      this.$nextTick(() => {
+        this.$refs.addOrUpdate.init(patientId, patientName)
+      })
+    },
+    // 修改
+    updateHandle (ghId) {
+      this.$confirm(`确定该用户已就诊?`, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        console.log(ghId)
+        this.$http({
+          url: this.$http.adornUrl('/sys/gh/status/' + ghId),
+          method: 'get',
+          data: this.$http.adornParams()
+        }).then(({data}) => {
+          if (data && data.code === 0) {
+            this.$message({
+              message: '操作成功',
+              type: 'success',
+              duration: 1500,
+              onClose: () => {
+                this.getDataList()
+              }
+            })
+          } else {
+            this.$message.error(data.msg)
+          }
+        })
+      }).catch(() => {})
+      this.getDataList()
+    },
+    // 删除
+    deleteHandle (id) {
+      var ghIds = id ? [id] : this.dataListSelections.map(item => {
+        return item.ghId
+      })
+      this.$confirm(`确定对该挂号信息进行[${id ? '删除' : '批量删除'}]操作?`, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.$http({
+          url: this.$http.adornUrl('/sys/gh/delete'),
+          method: 'post',
+          data: this.$http.adornData(ghIds, false)
+        }).then(({data}) => {
+          if (data && data.code === 0) {
+            this.$message({
+              message: '操作成功',
+              type: 'success',
+              duration: 1500,
+              onClose: () => {
+                this.getDataList()
+              }
+            })
+          } else {
+            this.$message.error(data.msg)
+          }
+        })
+      }).catch(() => {})
+    }
+  }
+}
+</script>
